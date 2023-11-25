3b45b4a080e8857ecf8f7355c9f3a175ac65453c
Andreas Steiner <andreassteiner@gmx.de>
2023-11-25 09:45:22 +0100

    Adds file upload (csv parsing not implemented yet).

--- src/app/components/configuration-prerecording-list-sidebar/configuration-prerecording-list-sidebar.component.ts
+++ src/app/components/configuration-prerecording-list-sidebar/configuration-prerecording-list-sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewContainerRef } from '@angular/core';
+import { Component, ViewChild, ViewContainerRef } from '@angular/core';
 import { ModalHandle, ModalInstance, ModalService } from '../../modules/modal/modal.service';
 import { PrerecordingService, Recording } from '../../states/prerecording.service';
 import {
@@ -52,8 +52,30 @@ export class ConfigurationPrerecordingListSidebarComponent implements ModalInsta
     downloadCSV(data, 'preprecordings.csv');
   }
 
-  import() {
-    alert('Not implemented yet.');
+  import(event: MouseEvent) {
+    const div = (event.target as HTMLElement).closest('div')!;
+    const input = document.createElement('input');
+    input.setAttribute('type', 'file');
+    input.setAttribute('accept', '.csv');
+    input.setAttribute('multiple', 'false');
+    input.style.display = 'none';
+    input.addEventListener('change', () => {
+      if (input.files && input.files.length > 0) {
+        const reader = new FileReader();
+        reader.onload = function (e) {
+          const csvContent = e.target?.result as string;
+          const csvRows = csvContent.split('\n');
+          const csvData: string[][] = csvRows.map(row => row.split(',').map(unescapeAndUnquote));
+          console.log('Parsed CSV data:', csvData);
+          // --> use https://www.npmjs.com/package/csv-parse
+        };
+        reader.readAsText(input.files[0]);
+      }
+      input.remove();
+      alert('not implemented yet.');
+    })
+    div.appendChild(input);
+    input.click();
   }
 }
 
@@ -75,4 +97,12 @@ function escapeAndQuote(field: string): string {
     return `"${escapedField}"`;
   }
   return escapedField;
+}
+
+function unescapeAndUnquote(field: string): string {
+  const unescapedField = field.replace(/""/g, '"');
+  if (unescapedField.includes(',') || unescapedField.includes('\n') || unescapedField.includes('\r')) {
+    return unescapedField.substring(1, unescapedField.length - 1);
+  }
+  return unescapedField;
 }
