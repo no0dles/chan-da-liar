80c8bee20e6bb00f710ad9cfdf05cc34b19989ca
Pascal <pascal@bertschi.io>
2024-01-31 19:24:40 +0100

    fix: update system prompt

--- server/mobile-server/src/chatgpt.ts
+++ server/mobile-server/src/chatgpt.ts
@@ -37,7 +37,7 @@ export function ask(recognizer: OngoingRecognizer, messages: ChatCompletionMessa
     model: apiModel as 'gpt-3.5-turbo',
     stream: true,
     messages,
-    functions,
+    functions: [],
     function_call: 'auto',
   })
   stream.on('content', async (delta) => {
